253ea0b7ecf1f9fab3509c190849c02f73b9e76d
Travis CI <stephane.langlois@scopyleft.fr>
2017-07-31 14:23:50 +0200

    Create home page

--- html.js
+++ html.js
@@ -1,29 +1,39 @@
-  document.addEventListener("DOMContentLoaded", function(event) {
-      const form = document.querySelector('form')
-      form.onsubmit = (event) => {
-          event.preventDefault()
-          const query = document.querySelector('input').value
-          const imgsSelect = ImgsDb.findByName(query)
-          displayImgs(imgsSelect)
-      }
-  })
+document.addEventListener("DOMContentLoaded", function(event) {
+    const form = document.querySelector('form')
+    const aGaleries = document.querySelectorAll('a.gallery')
 
-  const displayImgs = (imgsSelect) => {
-      const area = document.querySelector('.imgs')
-      const tpl = document.querySelector('#img')
-      area.innerHTML = ''
-      imgsSelect.map(item => {
-          let a = tpl.content.querySelector('a')
-          a.href = item.fileName.img
-          a.setAttribute('data-caption', `${item.Fr} - <i>${item.Lat}</i>`)
-          let img = tpl.content.querySelector('img')
-          img.src = item.fileName.thumbnail
-          img.title = item.model
-          let pLatin = tpl.content.querySelector('p.latin')
-          let pFr = tpl.content.querySelector('p.nom')
-          pLatin.innerText = item.Lat
-          pFr.innerText = item.Fr
-          let clone = document.importNode(tpl.content, true)
-          area.appendChild(clone)
-      })
-  }
+    form.onsubmit = (event) => {
+        event.preventDefault()
+        const query = document.querySelector('input').value
+        const imgsSelect = ImgsDb.findByName(query)
+        displayImgs(imgsSelect)
+    }
+
+    Array.from(aGaleries).map(a =>
+        a.onclick = (event) => {
+            const query = event.target.innerText
+            const imgsSelect = ImgsDb.findByName(query)
+            displayImgs(imgsSelect)
+        }
+    )
+})
+
+const displayImgs = (imgsSelect) => {
+    const area = document.querySelector('.imgs')
+    const tpl = document.querySelector('#img')
+    area.innerHTML = ''
+    imgsSelect.map(item => {
+        let a = tpl.content.querySelector('a')
+        a.href = item.fileName.img
+        a.setAttribute('data-caption', `${item.Fr} - <i>${item.Lat}</i>`)
+        let img = tpl.content.querySelector('img')
+        img.src = item.fileName.thumbnail
+        img.title = item.model
+        let pLatin = tpl.content.querySelector('p.latin')
+        let pFr = tpl.content.querySelector('p.nom')
+        pLatin.innerText = item.Lat
+        pFr.innerText = item.Fr
+        let clone = document.importNode(tpl.content, true)
+        area.appendChild(clone)
+    })
+}
